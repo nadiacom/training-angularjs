adde28662fff16601cf94caed8beb9172909a07c
ncomptier <ncomptier@e-biz.fr>
2017-06-02 17:04:58 +0200

    [Refacto] clear pagination code

--- src/app/cdb/dashboard/pagination/pagination.component.js
+++ src/app/cdb/dashboard/pagination/pagination.component.js
@@ -1,42 +1,6 @@
 (function() {
     'use strict';
 
-    var MyPage = MyPage || {};
-
-    MyPage.helpers = {
-        getPagination: function(pageSize, currentPage, totalNbComputer) {
-          var values = [];
-          var NB_PAGINATION = 10;
-          //CALCULATE NUMBER OF PAGINATION
-          var totalPages = 1, reste = 0, quotient = 0;
-          if (totalNbComputer > pageSize) {
-              //number of pagination to display computer list
-              reste = totalNbComputer % pageSize;
-              quotient = reste != 0
-                      ? totalNbComputer / pageSize + 1
-                      : totalNbComputer / pageSize;
-              totalPages = quotient;
-          }
-
-          //DISPLAYED PAGINATIONS
-          var pgStart = Math.max(currentPage - NB_PAGINATION / 2, 1);
-          var pgEnd = pgStart + NB_PAGINATION;
-          if (pgEnd > totalPages + 1) {
-              var diff = pgEnd - totalPages;
-              pgStart -= diff - 1;
-              if (pgStart < 1) {
-                  pgStart = 1;
-              }
-              pgEnd = totalPages + 1;
-          }
-          values[0] = pgStart;
-          values[1] = pgEnd;
-          values[2] = totalPages;
-          console.log(values[0]+ " " + values[1] + " "+ values[2]);
-          return values;
-        }
-    };
-
     angular.module('app.cdb')
         .component('pageComponent', {
             templateUrl: 'src/app/cdb/dashboard/pagination/pagination.html',
@@ -59,18 +23,27 @@
           vm.currentPage = 1;
           vm.pageSize = 10;
           //Calculate data
-          vm.myValues = MyPage.helpers.getPagination(vm.pageSize, vm.currentPage, vm.totalNbComputer);
-          vm.pgStart = vm.myValues[0];
-          vm.pgEnd = vm.myValues[1];
-          vm.totalPagination = vm.myValues[2];
-          vm.isLastPage = (vm.currentPage == vm.totalPagination);
+          vm.getPagination();
           vm.showPagination();
-        }
-
-        vm.numPages = function () {
-          return Math.ceil(vm.totalNbComputer / $scope.pageSize);
         };
 
+        vm.getPagination = function(){
+          var NB_PAGINATION = 10;
+          vm.totalPagination = 1;
+          if (vm.totalNbComputer > vm.pageSize) {
+              vm.totalPagination = Math.ceil(vm.totalNbComputer / vm.pageSize);
+          }
+          vm.pgStart = Math.max(vm.currentPage - NB_PAGINATION / 2, 1);
+          vm.pgEnd = vm.pgStart + NB_PAGINATION;
+          if (vm.pgEnd > vm.totalPagination + 1) {
+              var diff = vm.pgEnd - vm.totalPagination;
+              vm.pgStart -= diff - 1;
+              if (vm.pgStart < 1) {
+                  vm.pgStart = 1;
+              }
+              vm.pgEnd = vm.totalPagination + 1;
+          }
+        };
         vm.showPagination = function () {
           vm.visiblePages = [];
           for (var i = vm.pgStart; i< vm.pgEnd; i++){
@@ -78,13 +51,13 @@
           }
           vm.isLastPage = (vm.currentPage == vm.totalPagination);
         };
+        
+        //DASHBOARD CALLBACKS
         //On click pagination index
         vm.selectPage = function(index) {
             //Reload pagination
             vm.currentPage = index;
-            vm.myValues = MyPage.helpers.getPagination(vm.pageSize, vm.currentPage, vm.totalNbComputer);
-            vm.pgStart = vm.myValues[0];
-            vm.pgEnd = vm.myValues[1];
+            vm.getPagination();
             vm.showPagination();
             //Dashboard callback
             vm.events.onPageChange(vm.currentPage);
@@ -92,7 +65,11 @@
         //On click page size choice index
         vm.selectPageSize = function(size) {
             vm.pageSize = size;
+            //Reload pagination
+            vm.currentPage = 1;
+            vm.getPagination();
+            vm.showPagination();
             vm.events.onPageSizeChange(vm.pageSize);
-        }
+        };
     }
 })();
